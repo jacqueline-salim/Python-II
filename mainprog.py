21430dbf57f5c13e56896381b189bed417684461
jacqueline-salim <jacquelinelimjy@gmail.com>
2024-07-17 10:09:50 +0800

    Update mainprog.py
    Added an employee

--- mainprog.py
+++ mainprog.py
@@ -1,3 +1,9 @@
 from empClass import Employee
-emp4 = Employee("Xavier", "Ho", "M7777"
+emp4 = Employee("Xavier", "Ho", "M7777")
 print(emp4.first_name)
+
+emp5 = Employee("Jessie", "Gho", "M1234")
+print(emp5.last_name)
+
+print(emp1.FullName, emp2.FullName, emp3.FullName, emp4.FullName, emp5.FullName)
+
